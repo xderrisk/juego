922e639a892366e78b6f569f2bc1ce1412216ffa
Sergio Galarza <xderrisk@gmail.com>
2024-06-19 09:18:14 -0500

    la pelota ya no se sale del limite

--- src/code/main.py
+++ src/code/main.py
@@ -1,39 +1,41 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((720, 480))
+screen = pygame.display.set_mode((240, 480))
+pygame.display.set_caption("Juego")
 clock = pygame.time.Clock()
 running = True
+
+limite = pygame.Rect(25, 25, 190, 430)
+jugador = pygame.Vector2(120,35)
+gravedad = 25
 dt = 0
-suelo = pygame.Rect(0, screen.get_height() -50, screen.get_width(), 50)
-jugador = pygame.Vector2(screen.get_height() / 2, screen.get_width() / 2)
-gravedad = 400
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("purple")
-    
-    pygame.draw.rect(screen, "green", suelo)
-    pygame.draw.circle(screen, "orange", jugador, 40)
-
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        jugador.y -= 1000 * dt
     if keys[pygame.K_s]:
-        jugador.y += 300 * dt
+        jugador.y += 300 * dt #cae
     if keys[pygame.K_a]:
-        jugador.x -= 300 * dt
+        jugador.x -= 300 * dt #izq
     if keys[pygame.K_d]:
-        jugador.x += 300 * dt
+        jugador.x += 300 * dt #der
 
     jugador.y += gravedad * dt
-    if jugador.y + 40 > suelo.top:
-        jugador.y = suelo.top - 40
+    if jugador.y + 10 > limite.bottom:
+        jugador.y = limite.bottom - 10
+    if jugador.x - 10 < limite.left:
+        jugador.x = limite.left + 10
+    if jugador.x + 10 > limite.right:
+        jugador.x = limite.right - 10
 
+    screen.fill('black')
     pygame.display.flip()
+    pygame.draw.rect(screen, 'red', limite, 1)
+    pygame.draw.circle(screen, 'orange', jugador, 10)
     clock.tick(60)
     dt = clock.tick(60) / 1000
 
